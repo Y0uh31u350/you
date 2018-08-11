0bae7c6731921d52ffb54e516a7671c47299a9eb
Y9uhei9031 <youhei.ueno9031@gmail.com>
2018-08-11 20:14:53 +0900

    ユーザー名のカラムを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 require 'sidekiq/web'
+
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   mount Sidekiq::Web, at: '/sidekiq'
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
